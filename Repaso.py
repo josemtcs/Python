65bb07a5823b5e1a83ca85a7720a18717caf7b3f
josem <jose20030707@gmail.com>
2024-07-27 18:19:01 -0500

    Nuevo archivo y el repaso

---
+++ Repaso.py
@@ -0,0 +1,8 @@
+A = str(input("Ingrese un Algo: "))
+B = int(input("Ingrese un Numero: "))
+C = float(input("Ingrese un decimal: "))
+D = bool(input("Ingrese x o y "))
+x= True
+y= False
+
+print(A,B,C,D)
